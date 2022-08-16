079cd54ca39d0ce911b69f3aff2b63a29c3e3998
Jinsy05 <110526639+Jinsy05@users.noreply.github.com>
2022-08-16 15:41:06 +0530

    Update jenkins_install.sh

--- scripts/jenkins/jenkins_install.sh
+++ scripts/jenkins/jenkins_install.sh
@@ -1,4 +1,5 @@
 apt-get update && apt-get install -y --no-install-recommends openjdk-11-jdk wget git  ca-certificates lsb-release gnupg curl zip && rm -rf /var/lib/apt/lists/*
+
 wget https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/2.10.0/jenkins-plugin-manager-2.10.0.jar
 
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
@@ -10,7 +11,13 @@ apt-get install -y jenkins
 #sleep 100
 java -jar jenkins-plugin-manager-2.10.0.jar --war  /usr/share/java/jenkins.war --plugin-file /usr/share/jenkins/ref/plugins.txt  --plugin-download-directory /var/jenkins_home/plugins --verbose
 
-
-
 java  -Djenkins.install.runSetupWizard=false  -jar /usr/share/java/jenkins.war  &
+sleep 10
+
+
+
+wget http://localhost:8080/jnlpJars/jenkins-cli.jar
+sleep 10
+
+java -jar jenkins-cli.jar -s http://localhost:8080 create-job Terraform_Template_Job15 < /usr/share/jenkins/ref/Terraform_Template_Job.xml
 sleep "infinity"
