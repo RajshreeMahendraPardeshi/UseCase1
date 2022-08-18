548a6bf8ca5f3a250ef90602390fc650a7d91c51
Jinsy05 <110526639+Jinsy05@users.noreply.github.com>
2022-08-18 23:28:57 +0530

    Update ec2.tf

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -1,26 +1,26 @@
-resource "aws_instance" "webserver" {
-  ami                    = "ami-090fa75af13c156b4"
-  instance_type          = "t2.micro"
-  availability_zone      = "us-east-1a"
-  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
-  subnet_id              = aws_subnet.web_sub.id
-  
+
+resource "aws_instance" "jump_box" {
+  ami           = "ami-090fa75af13c156b4"
+  instance_type = "${var.instance_type}"
+  key_name      = "iac_key"
+
+  subnet_id              = aws_subnet.pub_sub.id
+  vpc_security_group_ids = [aws_security_group.general_sg.id, aws_security_group.bastion_sg.id]
+
   tags = {
     Project = "IAC"
   }
-
 }
 
-resource "aws_instance" "dbserver" {
-  ami                    = "ami-090fa75af13c156b4"
-  instance_type          = "t2.micro"
-  availability_zone      = "us-east-1a"
-  vpc_security_group_ids = [aws_security_group.database-sg.id]
-  subnet_id              = aws_subnet.db_sub.id
-  
+resource "aws_instance" "app_instance" {
+  ami           = "ami-090fa75af13c156b4"
+  instance_type = "${var.instance_type}"
+  key_name      = "iac_key"
+
+  subnet_id              = aws_subnet.prv_sub.id
+  vpc_security_group_ids = [aws_security_group.general_sg.id, aws_security_group.app_sg.id]
+
   tags = {
     Project = "IAC"
   }
-
 }
-
