5a4cbeaf473bd13bd05b682bca914dcd397305f1
Jinsy05 <110526639+Jinsy05@users.noreply.github.com>
2022-08-18 22:05:18 +0530

    Update ec2.tf

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "webserver" {
-  ami                    = "-"
+  ami                    = "ami-090fa75af13c156b4"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1a"
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
