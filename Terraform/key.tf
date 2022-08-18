0292a511bc3979f6090a38303d2a93134188fa93
Jinsy05 <110526639+Jinsy05@users.noreply.github.com>
2022-08-18 23:22:22 +0530

    Update key.tf

--- Terraform/key.tf
+++ Terraform/key.tf
@@ -2,3 +2,8 @@ resource "tls_private_key" "rsa_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "iac_key"
+  public_key = tls_private_key.rsa_key.public_key_openssh
+}
