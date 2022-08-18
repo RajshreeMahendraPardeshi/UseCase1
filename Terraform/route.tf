9800823971c72c2bb1626d7094ae0a2f194462c7
Jinsy05 <110526639+Jinsy05@users.noreply.github.com>
2022-08-18 13:44:13 +0530

    Update route.tf

--- Terraform/route.tf
+++ Terraform/route.tf
@@ -15,3 +15,22 @@ resource "aws_route_table_association" "rt_assoc" {
   subnet_id      = aws_subnet.pub_sub.id
   route_table_id = aws_route_table.pub_rt.id
 }
+
+
+resource "aws_route_table" "prv_rt" {
+  vpc_id = aws_vpc.terraform_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
+
+  tags = {
+    Project = "IAC"
+  }
+}
+
+resource "aws_route_table_association" "prv_rt_assoc" {
+  subnet_id      = aws_subnet.prv_sub.id
+  route_table_id = aws_route_table.prv_rt.id
+}
