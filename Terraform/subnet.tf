2acd988d7488f6958ded5655a0b4d02423fda220
Jinsy05 <110526639+Jinsy05@users.noreply.github.com>
2022-08-18 14:18:15 +0530

    Update subnet.tf

--- Terraform/subnet.tf
+++ Terraform/subnet.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "pub_sub" {
+resource "aws_subnet" "web_sub" {
   vpc_id                  = aws_vpc.terraform_vpc.id
   cidr_block              = "172.16.1.0/24"
   availability_zone       = "us-east-1a"
@@ -9,7 +9,7 @@ resource "aws_subnet" "pub_sub" {
   }
 }
 
-resource "aws_subnet" "prv_sub" {
+resource "aws_subnet" "app_sub" {
   vpc_id                  = aws_vpc.terraform_vpc.id
   cidr_block              = "172.16.4.0/24"
   availability_zone       = "us-east-1a"
@@ -19,3 +19,16 @@ resource "aws_subnet" "prv_sub" {
     Project = "IAC"
   }
 }
+
+resource "aws_subnet" "db_sub" {
+  vpc_id                  = aws_vpc.terraform_vpc.id
+  cidr_block              = "172.16.5.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Project = "IAC"
+  }
+}
+
+
