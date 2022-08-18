626acc0dd957b6da9baac812a543d87cd8cb955d
Jinsy05 <110526639+Jinsy05@users.noreply.github.com>
2022-08-18 23:20:44 +0530

    Update SG.tf

--- Terraform/SG.tf
+++ Terraform/SG.tf
@@ -1,76 +1,77 @@
-resource "aws_security_group" "web-sg" {
-  name        = "Web-SG"
-  description = "Allow HTTP inbound traffic"
+resource "aws_security_group" "general_sg" {
+  description = "HTTP egress to anywhere"
   vpc_id      = aws_vpc.terraform_vpc.id
-
-  ingress {
-    description = "HTTP from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   tags = {
     Project = "IAC"
   }
 }
 
-# Create Web Server Security Group
-resource "aws_security_group" "webserver-sg" {
-  name        = "Webserver-SG"
-  description = "Allow inbound traffic from ALB"
+resource "aws_security_group" "bastion_sg" {
+  description = "SSH ingress to Bastion and SSH egress to App"
   vpc_id      = aws_vpc.terraform_vpc.id
-
-  ingress {
-    description     = "Allow traffic from web layer"
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.web-sg.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   tags = {
     Project = "IAC"
   }
 }
 
-# Create Database Security Group
-resource "aws_security_group" "database-sg" {
-  name        = "Database-SG"
-  description = "Allow inbound traffic from application layer"
+resource "aws_security_group" "app_sg" {
+  description = "SSH ingress from Bastion and all TCP traffic ingress from ALB Security Group"
   vpc_id      = aws_vpc.terraform_vpc.id
-
-  ingress {
-    description     = "Allow traffic from application layer"
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.webserver-sg.id]
-  }
-
-  egress {
-    from_port   = 32768
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
-    Project = "IAC"
+    Project = "IAC
   }
+}
+
+#EGRESS RULES
+
+
+resource "aws_security_group_rule" "out_http" {
+  type              = "egress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.general_sg.id
+}
+
+resource "aws_security_group_rule" "out_ssh_bastion" {
+  type                     = "egress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.bastion_sg.id
+  source_security_group_id = aws_security_group.app_sg.id
+}
+
+resource "aws_security_group_rule" "out_http_app" {
+  type              = "egress"
+  description       = "Allow TCP internet traffic egress from app layer"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.app_sg.id
+}
+
+#INGRESS RULES
+
+resource "aws_security_group_rule" "in_ssh_bastion_from_anywhere" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.bastion_sg.id
+}
+
+resource "aws_security_group_rule" "in_ssh_app_from_bastion" {
+  type                     = "ingress"
+  description              = "Allow SSH from a Bastion Security Group"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.app_sg.id
+  source_security_group_id = aws_security_group.bastion_sg.id
 }
