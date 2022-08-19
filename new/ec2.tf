85207de1e855716fa7cf7ae8191b2e4b2883c288
Jinsy05 <110526639+Jinsy05@users.noreply.github.com>
2022-08-19 10:11:31 +0530

    Update ec2.tf

--- new/ec2.tf
+++ new/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "appserver" {
   ami           = "ami-090fa75af13c156b4"
   instance_type = var.instance_type
-  key_name      = "iac_key"
+  #key_name      = "iac_key"
 
   subnet_id              = aws_subnet.web_sub.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
@@ -14,7 +14,7 @@ resource "aws_instance" "appserver" {
 resource "aws_instance" "webserver" {
   ami           = "ami-090fa75af13c156b4"
   instance_type = var.instance_type
-  key_name      = "iac_key"
+  #key_name      = "iac_key"
 
   subnet_id              = aws_subnet.app_sub.id
   vpc_security_group_ids = [aws_security_group.app_sg.id]
@@ -27,7 +27,7 @@ resource "aws_instance" "webserver" {
 resource "aws_instance" "dbserver" {
   ami           = "ami-090fa75af13c156b4"
   instance_type = var.instance_type
-  key_name      = "iac_key"
+  #key_name      = "iac_key"
 
   subnet_id              = aws_subnet.db_sub.id
   vpc_security_group_ids = [aws_security_group.db_sg.id]
