e963543eaeb6b3c752175e452d13beba40e7c39a
RajshreeMahendraPardeshi <r.mahendra.pardeshi@accenture.com>
2022-08-23 06:53:59 +0000

    removed files

--- Terraform/igw.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.terraform_vpc.id
-
-  tags = {
-    Project = "IAC"
-  }
-}
--- Terraform/key.tf
+++
@@ -1,9 +0,0 @@
-resource "tls_private_key" "rsa_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "key_pair" {
-  key_name   = "iac_key"
-  public_key = tls_private_key.rsa_key.public_key_openssh
-}
